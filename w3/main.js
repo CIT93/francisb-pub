a9824427490b654cdbf459cf0aa12ec5abad22eb
Francis Belocura <francisclarenceb@gmail.com>
2024-01-25 22:27:29 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -25,3 +25,24 @@ let carbonFootprintPoints = 0;
 
 determineHouseHoldPts(3)
 determineHouseHoldPts(4)
+
+function determineHomeSizePts(homeSize) {
+    if (homeSize === "large") {
+        impactScore += 10;
+    } else if (homeSize === "medium") {
+        impactScore += 7;
+    } else if (homeSize === "small") {
+        impactScore += 4;
+    } else if (homeSize === "apartment") {
+        impactScore += 2;
+    }
+    console.log(`Based on the size of the home being ${homeSize} the points would be ${impactScore}`)
+}
+
+let impactScore = 0;
+
+determineHomeSizePts("large")
+determineHomeSizePts("medium")
+determineHomeSizePts("small")
+determineHomeSizePts("apartment")
+
