6a70b50eb1e08aa651f266fa15943c05caf93895
Francis Belocura <francisclarenceb@gmail.com>
2024-02-22 19:50:53 -0800

    my attempt at the code, before I review other students code

--- w7/main.js
+++ w7/main.js
@@ -1,43 +1,9 @@
-import {renderTbl} from "./render.js";
+import { renderTbl } from "./render.js";
+import { determineHomeSizePts, determineHouseHoldPts } from "./cfootprint.js";
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const cfpData = [];
-
-function determineHomeSizePts(homeSize) {
-    let impactScore = 0;
-    if (homeSize === "large") {
-        impactScore += 10;
-    } else if (homeSize === "medium") {
-        impactScore += 7;
-    } else if (homeSize === "small") {
-        impactScore += 4;
-    } else if (homeSize === "apartment") {
-        impactScore += 2;
-    }
-    return impactScore;
-}
-
-
-function determineHouseHoldPts(numberInHousehold) {
-    let houseHoldPoints = 0;
-    if (numberInHousehold === 1) {
-        houseHoldPoints = 14;
-    } else if (numberInHousehold === 2) {
-        houseHoldPoints = 12;
-    } else if (numberInHousehold === 3) {
-        houseHoldPoints = 10;
-    } else if (numberInHousehold === 4) {
-        houseHoldPoints = 8;
-    } else if (numberInHousehold === 5) {
-        houseHoldPoints = 6;
-    } else if (numberInHousehold === 6) {
-        houseHoldPoints = 4;
-    } else if (numberInHousehold > 6) {
-        houseHoldPoints = 2;
-    }
-    return houseHoldPoints;
-}
 
 console.log("global scope");
 
